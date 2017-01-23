c845460dd0953b569af9fe913332921e1ab03076
Tobias Schulz <tobias.schulz@digitalkraft.de>
2017-01-23 04:42:55 +0100

    initial release

---
+++ FakeExitcodeZero/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace FakeExitcodeZero
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var o = Assembly.GetExecutingAssembly().Location.Replace(".exe", ".original.exe");
+			var a = string.Join(" ", args.Select(_ => "\"" + _.Replace("\"", "\\\"") + "\""));
+			Console.WriteLine("Original Assembly: " + o);
+			Console.WriteLine("Original Arguments: " + a);
+
+			try
+			{
+				Process p = new Process();
+				p.StartInfo = new ProcessStartInfo
+				{
+					UseShellExecute = false,
+					FileName = o,
+					Arguments = a,
+				};
+				p.Start();
+				p.WaitForExit();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+
+			foreach (var arg in args)
+			{
+				if (arg.StartsWith("--aot=outfile=", StringComparison.Ordinal))
+				{
+					var path = arg.Split('=')[2].Split(',')[0];
+					Console.WriteLine("Create file: " + path);
+					if (!File.Exists(path) && !File.Exists(path + ".tmp")) File.Create(path);
+				}
+			}
+
+			Environment.Exit(0);
+		}
+	}
+}
